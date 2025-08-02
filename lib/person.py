9b16fd585997f7cc2078db0df72a1efcddeddd72
Jasec <jasec_7@hotmail.com>
2025-08-02 02:02:50 -0700

    complete python p3 attributes and properties

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -14,6 +16,27 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
+    def __init__(self, name = "name", job = "Admin"):
+        self.name = name
+        self.job = job
 
-class Person:
-    pass
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title() 
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in Person.APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
